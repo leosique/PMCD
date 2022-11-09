7b5865f024c20425e73d938edb0013aed695473d
Gouveia Raissa (CtP/ETS) <gor1ct@bosch.com>
2022-11-09 14:22:47 -0300

    login segurança e adm

--- development/UI/front-seguranca-adm/src/app/app-routing.module.ts
+++ development/UI/front-seguranca-adm/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginAdmComponent } from './login-adm/login-adm.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component:LoginAdmComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ development/UI/front-seguranca-adm/src/app/general-top-bar/general-top-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-general-top-bar',
+  templateUrl: './general-top-bar.component.html',
+  styleUrls: ['./general-top-bar.component.css']
+})
+export class GeneralTopBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
