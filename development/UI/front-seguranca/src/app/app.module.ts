e2174a47e96176c74e0cbdbd61e1a3ff8f09563a
Gouveia Raissa (CtP/ETS) <gor1ct@bosch.com>
2022-11-09 14:24:13 -0300

    login segurança e adm

--- development/UI/front-seguranca/src/app/app.module.ts
+++ development/UI/front-seguranca/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GeneralTopBarComponent } from './general-top-bar/general-top-bar.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { LoginSegurancaComponent } from './login-seguranca/login-seguranca.component';
+import { EntradasPendentesComponent } from './entradas-pendentes/entradas-pendentes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeneralTopBarComponent,
+    TopBarComponent,
+    LoginSegurancaComponent,
+    EntradasPendentesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: LoginSegurancaComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ development/UI/front-seguranca/src/app/entradas-pendentes/entradas-pendentes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entradas-pendentes',
+  templateUrl: './entradas-pendentes.component.html',
+  styleUrls: ['./entradas-pendentes.component.css']
+})
+export class EntradasPendentesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
