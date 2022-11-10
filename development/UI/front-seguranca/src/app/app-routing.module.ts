3a84bfbb69f0104ac129fb6ae633a1e08058c268
Gouveia Raissa (CtP/ETS) <gor1ct@bosch.com>
2022-11-10 16:48:42 -0300

    tabela pendentes

--- development/UI/front-seguranca/src/app/app-routing.module.ts
+++ development/UI/front-seguranca/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginSegurancaComponent } from './login-seguranca/login-seguranca.component';
+import { EntradasPendentesComponent } from './entradas-pendentes/entradas-pendentes.component';
 
 const routes: Routes = [
-  {path:'login', component:LoginSegurancaComponent}
+  {path:'login', component:LoginSegurancaComponent},
+  {path: 'entradas-pendentes', component:EntradasPendentesComponent}
 ];
 
 @NgModule({
--- development/UI/front-seguranca/src/app/entradas-pendentes/entradas-pendentes.component.ts
+++ development/UI/front-seguranca/src/app/entradas-pendentes/entradas-pendentes.component.ts
@@ -6,10 +6,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entradas-pendentes.component.css']
 })
 export class EntradasPendentesComponent implements OnInit {
+  // array: Array<Pendentes> = [];
+
+  // min:number = 0;
+  // max: number = 0;
+  // totalItems: number = 0;
+  // pages: number = 0;
+  // pagAtual:number = 1;
+  // itemsPerPage: number = 9;
+  // PagTotalArray: Array<number> = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  // ProxPag(){
+  //   if(this.pagAtual != this.pages){
+  //     this.pagAtual++;
+  //   }
+
+  //   this.loadPage();
+  // }
+
+  // PagAnt(){
+  //   if(this.pagAtual != 1){
+  //     this.pagAtual--;
+  //   }
+
+  //   this.loadPage();
+  // }
+  
+  // loadPage(){
+  //   this.min = (this.pagAtual-1)*this.itemsPerPage;
+  //   this.max = (this.pagAtual)*this.itemsPerPage;
+
+  //   this.ChangeBtnState();
+  // }
+
+  // ChangeBtnState(){
+  //   let btnProx = document.querySelector("#ProxBtn");
+  //   let btnAnt = document.querySelector("#AntBtn");
+
+  //   // btnProx
+  //   if(this.pagAtual == this.pages ){
+  //     btnProx?.setAttribute("style", "pointer-events: none;  opacity: 0.5;");
+  //   }else{
+  //     btnProx?.setAttribute("style", "pointer-events: all;");
+  //   }
+
+  //   // btnAnt
+  //   if(this.pagAtual == 1 || this.pagAtual == 0){
+  //     btnAnt?.setAttribute("style", "pointer-events: none; opacity: 0.5;");
+  //   }else{
+  //     btnAnt?.setAttribute("style", "pointer-events: all;");
+  //   }
+  // }
+
+  // SetItemsPerPage(){
+  //   this.itemsPerPage = Number((document.getElementById('showSelector') as HTMLInputElement).value);
+
+  //   // this.itemsPerPage = Number(showSelector.value);
+  //   console.log(this.itemsPerPage);
+    
+  //   this.loadPage();
+  //   this.CalculatePages();
+  // }
+
+  // CalculatePages(){
+  //   this.pages = Math.ceil(this.totalItems/this.itemsPerPage);
+
+  //   this.PagTotalArray = [];
+
+  //   for(let i = 1; i <= this.pages; i++){
+  //     this.PagTotalArray.push(i);
+  //   }
+  // }
+
+  // selectPage(page: number){
+  //   this.pagAtual = page;
+  //   this.loadPage();
+  // }
 
 }
