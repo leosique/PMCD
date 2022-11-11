201147b09cd1bbfe4ed710f0ee97a10cd95b315f
Gouveia Raissa (CtP/ETS) <gor1ct@bosch.com>
2022-11-11 13:36:59 -0300

    tables entradas pendentes e aprovadas

---
+++ development/UI/front-seguranca/src/app/entradas-aprovadas/entradas-aprovadas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entradas-aprovadas',
+  templateUrl: './entradas-aprovadas.component.html',
+  styleUrls: ['./entradas-aprovadas.component.css']
+})
+export class EntradasAprovadasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
