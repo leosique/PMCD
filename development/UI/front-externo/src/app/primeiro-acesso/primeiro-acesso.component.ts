e76f5585127c7c8759404f79a0e5492d18096a4c
Allan Kley <allanlkley@gmail.com>
2023-01-25 13:38:10 -0300

    verificação de campos primeio logina criado

--- development/UI/front-externo/src/app/primeiro-acesso/primeiro-acesso.component.ts
+++ development/UI/front-externo/src/app/primeiro-acesso/primeiro-acesso.component.ts
@@ -14,19 +14,96 @@ export class PrimeiroAcessoComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  mudarCorAzul(idTag : String){
-    var inp = document.querySelector('#'+idTag);
+  mudarCorAzul(idTag: String) {
+    var inp = document.querySelector('#' + idTag);
     inp?.setAttribute('style', 'border-bottom: .15em solid #2d7db5;');
   }
-  
-  mudarCorCinza(idTag : String){
-    var inp = document.querySelector('#'+idTag);
+
+  mudarCorCinza(idTag: String) {
+    var inp = document.querySelector('#' + idTag);
     inp?.setAttribute('style', 'border-bottom: .15em solid #C1C7CC;');
   }
 
-  EditarSenha(){
+  verificarCampos() {
+    let cnpj = (document.getElementById("cnpj") as HTMLInputElement).value;
+    let senhaAntiga = (document.getElementById("senhaAntiga") as HTMLInputElement).value;
+    let senhaNova = (document.getElementById("senhaNova") as HTMLInputElement).value;
+    let repetirSenha = (document.getElementById("repetirSenha") as HTMLInputElement).value;
+    let erro = document.getElementById("erro")
+    let mensagemErro = ""
+    let camposVazios = new Array()
+
+    if (cnpj == "") {
+      camposVazios.push("CNPJ");
+    }
+    if (senhaAntiga == "") {
+      camposVazios.push("Senha antiga");
+    }
+    if (senhaNova == "") {
+      camposVazios.push("Nova Senha");
+    }
+    if (repetirSenha == "") {
+      camposVazios.push("Repetir senha");
+    }
+
+    if (camposVazios.length > 0) {
+      mensagemErro = "Campos vazios: "
+
+      camposVazios.forEach((campo, index) => {
+
+        if (index == 0) {
+          mensagemErro += campo
+        } else {
+          mensagemErro += ", " + campo
+        }
+      });
+    }
+
+
+
+    if (erro != null) {
+      erro.style.display = "block";
+      erro.textContent = mensagemErro;
+    }
+
+    //this.EditarSenha();
+  }
+
+  EditarSenha() {
     console.log("oi");
-    
-    this.router.navigate(['cadastro-veiculo']);
+
+    let cnpj = document.getElementById("cnpj") as HTMLInputElement;
+    let senha = document.getElementById("senhaAntiga") as HTMLInputElement;
+    let senhaNova = document.getElementById("senhaNova") as HTMLInputElement;
+    let erro = document.getElementById("erro")
+
+
+    var data = JSON.stringify({
+      "cnpj": cnpj.value,
+      "senha": senha.value
+    });
+
+
+    var config = {
+      method: 'put',
+      url: 'https://localhost:7274/Transportadora/EditarPrimeiroAcesso/' + senhaNova.value,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: data
+    };
+
+    let instance = this;
+    localStorage.removeItem('authToken');
+    axios(config)
+      .then(function (response) {
+        console.log(response.data);
+
+        //instance.router.navigate(['cadastro-veiculo']);
+
+      })
+      .catch(function (error) {
+      })
+
   }
 }
