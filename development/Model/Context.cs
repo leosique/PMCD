1cf53f82510295cf85dda9722ae5f3bdcc518f8c
Allan Kley <allanlkley@gmail.com>
2023-01-25 09:39:35 -0300

    a

--- development/Model/Context.cs
+++ development/Model/Context.cs
@@ -20,7 +20,7 @@ public partial class Context : DbContext
         string computador = Environment.MachineName;
         computador += "\\SQLEXPRESS";
         string database = "PMCD";
-        optionsBuilder.UseSqlServer("Server=" + computador + "\\SQLEXPRESS" + ";Database=" + database + ";Trusted_Connection=True;TrustServerCertificate=True;");
+        optionsBuilder.UseSqlServer("Server=" + computador + ";Database=" + database + ";Trusted_Connection=True;TrustServerCertificate=True;");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
--- development/UI/front-externo/src/app/login-externo/login-externo.component.ts
+++ development/UI/front-externo/src/app/login-externo/login-externo.component.ts
@@ -17,7 +17,7 @@ export class LoginExternoComponent implements OnInit {
     var inp = document.querySelector('#'+idTag);
     inp?.setAttribute('style', 'border-bottom: .15em solid #2d7db5;');
   }
-  
+
   mudarCorCinza(idTag : String){
     var inp = document.querySelector('#'+idTag);
     inp?.setAttribute('style', 'border-bottom: .15em solid #C1C7CC;');
