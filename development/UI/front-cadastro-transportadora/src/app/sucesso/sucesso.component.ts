d44786a8b1ebba81a14f86f667c9994f25febaa2
Allan Kley <allanlkley@gmail.com>
2023-01-26 10:32:49 -0300

    cadastro de transportadora

---
+++ development/UI/front-cadastro-transportadora/src/app/sucesso/sucesso.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sucesso',
+  templateUrl: './sucesso.component.html',
+  styleUrls: ['./sucesso.component.css']
+})
+export class SucessoComponent {
+
+}
