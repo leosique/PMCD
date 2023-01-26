14a36686f8184e1d6dca67448deaa6d96ed6d008
RaissaSabino <raissa-sabino@hotmail.com>
2023-01-26 16:08:31 -0300

    nao sei mais

--- development/UI/front-externo/src/app/cadastro-adicionais/cadastro-adicionais.component.ts
+++ development/UI/front-externo/src/app/cadastro-adicionais/cadastro-adicionais.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 export interface DialogData{
   ajudante: string;
@@ -16,4 +17,32 @@ export class CadastroAdicionaisComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  registerEntrega(){
+    var notaFiscal = (document.getElementById('nota') as HTMLInputElement).value;
+    var dataEntrega = (document.getElementById('data') as HTMLInputElement).value;
+
+    var data = JSON.stringify({
+      "notaFiscal": notaFiscal,
+      "dataEntrega": dataEntrega,
+    });
+    
+    var config = {
+      method: 'post',
+      url: 'http://localhost:5265/entrega/salvar',
+      headers: { 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+    
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      alert("Benefiário registrado com sucesso!");
+    })
+    .catch(function (error) {
+      alert(error);
+      console.log(error);
+    });
+  }
 }
