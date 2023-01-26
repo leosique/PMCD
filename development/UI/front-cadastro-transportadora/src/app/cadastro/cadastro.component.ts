e5a3b09a065219be03fc997f04656cb0189a8d0a
Allan Kley <allanlkley@gmail.com>
2023-01-26 13:26:21 -0300

    cadastro de transportadora

--- development/UI/front-cadastro-transportadora/src/app/cadastro/cadastro.component.ts
+++ development/UI/front-cadastro-transportadora/src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import axios from "axios";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,5 +8,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent {
+  constructor(private router: Router) { }
 
+  ngOnInit(): void {
+  }
+
+  verificaCampos(){
+    let cnpj = (document.getElementById("cnpj") as HTMLInputElement).value;
+    let nome = (document.getElementById("nome") as HTMLInputElement).value;
+    let erro = document.getElementById("erro") as HTMLElement
+
+    if(cnpj=="" || nome==""){
+      erro.style.display = "block";
+      erro.textContent = "Todos os campos devem ser preenchidos";
+    }else{
+      this.cadastro();
+    }
+    
+  }
+
+  cadastro() {
+    console.log("oi");
+
+    let cnpj = document.getElementById("cnpj") as HTMLInputElement;
+    let nome = document.getElementById("nome") as HTMLInputElement;
+
+    var data = JSON.stringify({
+      "cnpj": cnpj.value,
+      "nome": nome.value
+    });
+
+    var config = {
+      method: 'post',
+      url: 'https://localhost:7274/Transportadora/Salvar',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: data
+    };
+
+    let instance = this;
+
+    axios(config)
+      .then(function (response) {
+     
+        instance.router.navigate(['/sucesso']);
+        localStorage.setItem("cnpj", cnpj.value);
+       
+      })
+      .catch(function (error) {
+      })
+
+  }
 }
--- development/UI/front-cadastro-transportadora/src/app/sucesso/sucesso.component.ts
+++ development/UI/front-cadastro-transportadora/src/app/sucesso/sucesso.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import axios from "axios";
+import { Router } from '@angular/router';
+
+export interface Transportadora{
+  nome: string,
+  cnpj: string,
+  senha: string
+}
+
 
 @Component({
   selector: 'app-sucesso',
@@ -6,5 +15,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./sucesso.component.css']
 })
 export class SucessoComponent {
+  constructor(private router: Router) { }
+  transportadora: Transportadora = {nome:"", cnpj:"", senha:""}
 
+
+  ngOnInit(): void {
+    this.buscar();
+  }
+
+  buscar() {
+
+    var cnpj = localStorage.getItem("cnpj")
+
+    var config = {
+      method: 'get',
+      url: 'https://localhost:7274/Transportadora/BuscarCNPJ/' + cnpj,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    let instance = this;
+
+    axios(config)
+      .then(function (response) {
+     
+        instance.transportadora = response.data;
+       
+      })
+      .catch(function (error) {
+      })
+
+  }
 }
